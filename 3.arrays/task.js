9379057afcec25c617b8357d202d447e2203d45e
Artem <artembl487@gmail.com>
2023-01-08 10:20:21 +0300

    added solution in task.js 3.arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,11 @@
 function compareArrays(arr1, arr2) {
-  
+   return (arr1.length === arr2.length) && (arr1.every((item, index) => item === arr2[index]));
+   
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+   return users.filter((user)=> user.gender === gender).reduce((accumAge, ageUser, index, arrUsersFilter)=>{
+    accumAge += ageUser.age;
+   return (index === arrUsersFilter.length - 1) ? accumAge / arrUsersFilter.length : accumAge;
+  },0)
 }
